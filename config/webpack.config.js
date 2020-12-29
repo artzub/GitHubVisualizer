8f3c7c7b8d2ce9795d674d3a0b13ed8ae921178b
Artem Zubkov <artzub@gmail.com>
2020-12-29 15:26:45 +0700

    #68 Fixed some warning in console.

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -291,6 +291,7 @@ module.exports = function(webpackEnv) {
         // Support React Native Web
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
         'react-native': 'react-native-web',
+        'react-dom': '@hot-loader/react-dom',
         // Allows for better profiling with ReactDevTools
         ...(isEnvProductionProfile && {
           'react-dom$': 'react-dom/profiling',
--- src/themes/index.js
+++ src/themes/index.js
@@ -11,31 +11,31 @@ export const addTheme = (name, theme) => {
 addTheme(light.name, light);
 addTheme(dark.name, dark);
 
+const GlobalStyle = createGlobalStyle`
+  html,
+  body {
+    margin: 0;
+    padding: 0;
+    width: 100vw;
+    height: 100vh;
+    overflow: hidden;
+  }
+
+  //body {
+  //  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+  //    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+  //    sans-serif;
+  //  -webkit-font-smoothing: antialiased;
+  //  -moz-osx-font-smoothing: grayscale;
+  //}
+  //
+  //code {
+  //  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
+  //}
+`;
+
 export const getTheme = (name) => {
   const theme = createMuiTheme(themes[name] || themes.dark);
-
-  const GlobalStyle = createGlobalStyle`
-    html,
-    body {
-      margin: 0;
-      padding: 0;
-      width: 100vw;
-      height: 100vh;
-      overflow: hidden;
-    }
-
-    //body {
-    //  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
-    //    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
-    //    sans-serif;
-    //  -webkit-font-smoothing: antialiased;
-    //  -moz-osx-font-smoothing: grayscale;
-    //}
-    //
-    //code {
-    //  font-family: source-code-pro, Menlo, Monaco, Consolas, 'Courier New', monospace;
-    //}
-  `;
 
   return {
     theme,
