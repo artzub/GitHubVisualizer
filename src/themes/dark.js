d94ac8320a233bc38f1090f6b3520a4da9032e05
Artem Zubkov <artzub@gmail.com>
2021-01-05 21:21:52 +0700

    #75 changed background palette.

--- src/themes/dark.js
+++ src/themes/dark.js
@@ -7,5 +7,15 @@ export default {
     primary: {
       main: blue[200],
     },
+    background: {
+      default: '#282C34',
+      paper: '#21252B',
+
+      // default: '#1d283c',
+      // paper: '#2B4055',
+
+      // default: '#151D2B',
+      // paper: '#223344',
+    },
   },
 };
